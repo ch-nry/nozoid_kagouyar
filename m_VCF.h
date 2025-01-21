3ede5ea467be4b7f7dd911f98fd595aa5271adb8
chnry <ch@chnry.net>
2025-01-21 16:54:47 +0100

    ai optimisation

--- m_VCF.h
+++ m_VCF.h
@@ -24,93 +24,93 @@
 // Algorithms for Virtual Analog Synthesis †"
 // CMJ302
 
-inline float VCF1(uint32_t j, float fq, float input1) { //, float res, float mod1, float mod2) {
-    float tmp = 0.f;
-    float mod_fq = 0.f;
-    float mod_q = 0.f;
-	float res = g_pot_audio[k_VCF1_q] ;
+#define VCF1_FILTER_SCALE (1.f/1.3f)
+#define VCF1_FILTER_FEEDBACK 0.3f
+#define VCF1_Q_SCALE 4.5f
+#define VCF1_FREQ_CLAMP_MIN -128.f
+#define VCF1_FREQ_CLAMP_MAX 132.f
 
-    if(curent_config.c_VCF1_MOD1_TYPE == 0)
-        mod_fq += g_pot_audio[k_VCF1_mod1] * g_Modulation[curent_config.c_Modulation_Source[VCF1_MOD1]];
-    else
-        mod_q  += g_pot_audio[k_VCF1_mod1] * g_Modulation[curent_config.c_Modulation_Source[VCF1_MOD1]];
+inline float compute_filter_stage(float input, float last_input, float last_output, float g) {
+    const float output = (input + VCF1_FILTER_FEEDBACK * last_input) * VCF1_FILTER_SCALE;
+    return output + g * (output - last_output);
+}
 
-    if(curent_config.c_VCF1_MOD2_TYPE == 0)
-        mod_fq += g_pot_audio[k_VCF1_mod2] * g_Modulation[curent_config.c_Modulation_Source[VCF1_MOD2]];
-    else
-        mod_q  += g_pot_audio[k_VCF1_mod2] * g_Modulation[curent_config.c_Modulation_Source[VCF1_MOD2]];
+inline float VCF1(const uint32_t j, const float fq, float input1) {
+    // Modulation calculation
+    const float mod1_value = g_pot_audio[k_VCF1_mod1] * g_Modulation[curent_config.c_Modulation_Source[VCF1_MOD1]];
+    const float mod2_value = g_pot_audio[k_VCF1_mod2] * g_Modulation[curent_config.c_Modulation_Source[VCF1_MOD2]];
+    
+    const float mod_fq = (curent_config.c_VCF1_MOD1_TYPE == 0 ? mod1_value : 0.f) +
+                        (curent_config.c_VCF1_MOD2_TYPE == 0 ? mod2_value : 0.f);
+    
+    const float mod_q = (curent_config.c_VCF1_MOD1_TYPE != 0 ? mod1_value : 0.f) +
+                       (curent_config.c_VCF1_MOD2_TYPE != 0 ? mod2_value : 0.f);
 
-    float freq = fq+ (48.f*mod_fq) + (24.f*g_Modulation[MIDI_expression]);
-    tmp = curent_config.c_VCF1_pitch_TRACK;
-    freq += tmp * 0.5f * (allvoice[j].v_pitch-12.f);
-    freq = _fclamp(freq, -128.f, 132.f);
-    freq = CV2freq(freq);
+    // Frequency calculation
+    const float pitch_offset = curent_config.c_VCF1_pitch_TRACK * 0.5f * (allvoice[j].v_pitch - 12.f);
+    const float freq = CV2freq(_fclamp(
+        fq + (48.f * mod_fq) + (24.f * g_Modulation[MIDI_expression]) + pitch_offset,
+        VCF1_FREQ_CLAMP_MIN,
+        VCF1_FREQ_CLAMP_MAX
+    ));
 
-    float const omega = freq * (TWOPI_F/48000.f);
-    float const g = omega * (0.9892 + omega*(-0.4342 + omega*(0.1381 + omega * -0.0202)));
+    // Filter coefficients
+    const float omega = freq * (TWOPI_F/48000.f);
+    const float g = omega * (0.9892f + omega * (-0.4342f + omega * (0.1381f + omega * -0.0202f)));
+    
+    // Resonance calculation with frequency compensation
+    const float res = g_pot_audio[k_VCF1_q];
+    const float freq_comp = 1.0029f + omega * (0.0526f + omega * (-0.0926f + omega * 0.0218f));
+    const float Q = VCF1_Q_SCALE * _fclamp(res + mod_q, 0.f, 1.f) * freq_comp * 1.01f;
 
-    float Q = 4.5f * _fclamp(res + mod_q, 0., 1.);
-    Q *= 1.0029f + omega*(0.0526f + omega * (-0.0926f  + omega*0.0218f)); // resonance frequency compensation
-	Q *= 1.01f; // ??? c'est plus lent si je vire cette ligne!!!
+    // Input preprocessing
+    _fonepole(allvoice[j].v_VCF1_filter, input1, 12000.f * OneOverSR);
+    input1 = allvoice[j].v_VCF1_filter;
+    input1 = _tanh_clip(input1 - Q * (allvoice[j].v_VCF1_last_output4 - (0.5f * input1)));
 
-	_fonepole(allvoice[j].v_VCF1_filter, input1, 12000.f*OneOverSR); // on baisse les hautes frequences pour reduire le repliement ds la non linéarité
-	input1 = allvoice[j].v_VCF1_filter;
+    // Four cascaded one-pole filters
+    const float stage1 = compute_filter_stage(input1, allvoice[j].v_VCF1_last_input1, 
+                                            allvoice[j].v_VCF1_last_output1, g);
+    const float stage2 = compute_filter_stage(stage1, allvoice[j].v_VCF1_last_input2, 
+                                            allvoice[j].v_VCF1_last_output2, g);
+    const float stage3 = compute_filter_stage(stage2, allvoice[j].v_VCF1_last_input3, 
+                                            allvoice[j].v_VCF1_last_output3, g);
+    const float stage4 = compute_filter_stage(stage3, allvoice[j].v_VCF1_last_input4, 
+                                            allvoice[j].v_VCF1_last_output4, g);
 
-    float const feedback = Q * ( allvoice[j].v_VCF1_last_output4 - (0.5f * input1) ); // feedback
-
-    input1 -= feedback;
-    input1 = _tanh_clip(input1); // distortion
-
-    float output1 = (input1 + 0.3f * allvoice[j].v_VCF1_last_input1)*(1.f/1.3f); // 4 * 6dB filter
+    // Update filter states
     allvoice[j].v_VCF1_last_input1 = input1;
-    output1 -= allvoice[j].v_VCF1_last_output1;
-    output1 *= g;
-    output1 += allvoice[j].v_VCF1_last_output1;
-    allvoice[j].v_VCF1_last_output1 = output1;
+    allvoice[j].v_VCF1_last_output1 = stage1;
+    allvoice[j].v_VCF1_last_input2 = stage1;
+    allvoice[j].v_VCF1_last_output2 = stage2;
+    allvoice[j].v_VCF1_last_input3 = stage2;
+    allvoice[j].v_VCF1_last_output3 = stage3;
+    allvoice[j].v_VCF1_last_input4 = stage3;
+    allvoice[j].v_VCF1_last_output4 = stage4;
 
-    float output2 = (output1 + 0.3f * allvoice[j].v_VCF1_last_input2)*(1.f/1.3f);
-    allvoice[j].v_VCF1_last_input2 = output1;
-    output2 -= allvoice[j].v_VCF1_last_output2;
-    output2 *= g;
-    output2 += allvoice[j].v_VCF1_last_output2;
-    allvoice[j].v_VCF1_last_output2 = output2;
+    // Calculate intermediate values for filter types
+    const float output1 = (input1 + VCF1_FILTER_FEEDBACK * allvoice[j].v_VCF1_last_input1) * VCF1_FILTER_SCALE;
+    const float output2 = (stage1 + VCF1_FILTER_FEEDBACK * allvoice[j].v_VCF1_last_input2) * VCF1_FILTER_SCALE;
+    const float output3 = (stage2 + VCF1_FILTER_FEEDBACK * allvoice[j].v_VCF1_last_input3) * VCF1_FILTER_SCALE;
+    const float output4 = (stage3 + VCF1_FILTER_FEEDBACK * allvoice[j].v_VCF1_last_input4) * VCF1_FILTER_SCALE;
 
-    float output3 = (output2 + 0.3f * allvoice[j].v_VCF1_last_input3)*(1.f/1.3f);
-    allvoice[j].v_VCF1_last_input3 = output2;
-    output3 -= allvoice[j].v_VCF1_last_output3;
-    output3 *= g;
-    output3 += allvoice[j].v_VCF1_last_output3;
-    allvoice[j].v_VCF1_last_output3 = output3;
-
-    float output4 = (output3 + 0.3f * allvoice[j].v_VCF1_last_input4)*(1.f/1.3f);
-    allvoice[j].v_VCF1_last_input4 = output3;
-    output4 -= allvoice[j].v_VCF1_last_output4;
-    output4 *= g;
-    output4 += allvoice[j].v_VCF1_last_output4;
-    allvoice[j].v_VCF1_last_output4 = output4;
-
+    // Filter type selection
     switch (curent_config.c_VCF1_TYPE) {
-    case 0 :
-        tmp = output4;
-        break;
-    case 1 :
-        tmp = output2;
-        break;
-    case 2 :
-        tmp =   (output1 + output1 - output2 - output2);
-        break;
-    case 3 :
-        tmp = 4.f * (output2 - output3 - output3 + output4);
-        break;
-    case 4 :
-        tmp = (input1 - output1 - output1 + output2);
-        break;
-    case 5 :
-        tmp = (input1 - 4.f * (output1 + output3) + 6.f *  output2 + output4);
-        break;
+        case 0:
+            return stage4;
+        case 1:
+            return stage2;
+        case 2:
+            return (output1 + output1 - output2 - output2);
+        case 3:
+            return 4.f * (output2 - output3 - output3 + output4);
+        case 4:
+            return (input1 - output1 - output1 + output2);
+        case 5:
+            return (input1 - 4.f * (output1 + output3) + 6.f * output2 + output4);
+        default:
+            return 0.f;
     }
-
-	return tmp; // inclus dans le switch
 }
 
 float g_VCF2_last_input1 = 0.f;
@@ -118,19 +118,19 @@ float g_VCF2_last_input2 = 0.f;
 float g_VCF2_out = 0.f;
 
 inline void VCF2(float &input) {
-	if (curent_config.c_VCF2_TYPE == 2)
-		return; // pas de filtre
+    if (curent_config.c_VCF2_TYPE == 2) {
+        return;
+    }
 
-	float const fq = 127.f * ( g_pot_audio[k_VCF2_fq] += g_pot_increment[k_VCF2_fq]);
-	float const mod1 =  g_pot_audio[k_VCF2_mod] += g_pot_increment[k_VCF2_mod];
-    float filter_fq = mod1 * g_Modulation[curent_config.c_Modulation_Source[VCF2_MOD1]];
+    const float fq = 127.f * (g_pot_audio[k_VCF2_fq] += g_pot_increment[k_VCF2_fq]);
+    const float mod1 = g_pot_audio[k_VCF2_mod] += g_pot_increment[k_VCF2_mod];
+    const float mod_source = g_Modulation[curent_config.c_Modulation_Source[VCF2_MOD1]];
+    
+    const float filter_fq = CV2freq(
+        _fclamp(mod1 * mod_source * 48.f + fq, -127.f, 127.5f)
+    ) * (1.f/13000.f);
 
-    filter_fq *= 48.f;
-    filter_fq += fq;
-    filter_fq = _fclamp(filter_fq, -127.f, 127.5f);
-    filter_fq = CV2freq(filter_fq)*(1.f/13000.f);
-
-    _fonepole(g_VCF2_last_input1, input,              filter_fq);
+    _fonepole(g_VCF2_last_input1, input, filter_fq);
     _fonepole(g_VCF2_last_input2, g_VCF2_last_input1, filter_fq);
 
     input = curent_config.c_VCF2_TYPE ? (input - g_VCF2_last_input2) : g_VCF2_last_input2;
