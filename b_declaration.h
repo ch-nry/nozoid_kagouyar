4c3ff1a9826ae21b9f400084bc8bb452ed4a0b65
chnry <ch@chnry.net>
2026-05-28 16:22:13 +0200

    version 2.1!

--- b_declaration.h
+++ b_declaration.h
@@ -23,7 +23,7 @@
 // this code use an old and sligtly customized daisy lib.
 
 #define major_version 2
-#define minor_version  0
+#define minor_version  1
 #define software_led_version ((major_version << 8) + minor_version) // pour l'affichage de version en mode debug
 
 // Les 3 led MSB servent a indiquer la présence d'une calibration des CVs
